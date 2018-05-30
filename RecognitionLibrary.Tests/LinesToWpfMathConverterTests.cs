da31cfdab9c539965da356fd9a162aa819fd72fb
HavenDV <havendv@gmail.com>
2018-05-30 19:59:30 +1000

    Fixed tests

--- RecognitionLibrary.Tests/LinesToWpfMathConverterTests.cs
+++ RecognitionLibrary.Tests/LinesToWpfMathConverterTests.cs
@@ -12,7 +12,7 @@ namespace RecognitionLibrary.Tests
         public void ConvertTest()
         {
             BaseConvertTest(@"\sqrt{t} = \frac{S}{u}", "1.txt");
-            BaseConvertTest(@"p = \frac{1}{2} (a+b+c)", "2.txt");
+            BaseConvertTest(@"p = \frac{1}{2} ( a + b + c )", "2.txt");
         }
 
         private static void BaseConvertTest(string expected, string name)
