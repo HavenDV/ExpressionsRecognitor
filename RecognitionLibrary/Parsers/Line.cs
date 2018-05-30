8a7233885ecc4ed968957e11426da495ed302dc3
HavenDV <havendv@gmail.com>
2018-05-30 18:30:21 +1000

    Moved Line to separate class

---
+++ RecognitionLibrary/Parsers/Line.cs
@@ -0,0 +1,11 @@
+﻿namespace RecognitionLibrary.Parsers
+{
+    public class Line
+    {
+        public string Symbol { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
