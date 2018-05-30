45d58d83aa805ab05e2a7b92e574fadc8ddfbbcf
HavenDV <havendv@gmail.com>
2018-05-30 19:43:56 +1000

    Added show selection rectangles

--- RecognitionApplication/MainWindow.xaml.cs
+++ RecognitionApplication/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Windows;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 using Microsoft.Win32;
 using RecognitionLibrary.Converters;
 using RecognitionLibrary.Parsers;
@@ -9,6 +12,8 @@ namespace RecognitionApplication
 {
     public partial class MainWindow
     {
+        private string ImagePath { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -59,11 +64,46 @@ namespace RecognitionApplication
 
             var formula = LinesToWpfMathConverter.Convert(lines);
             FormulaTextBox.Text = formula;
+
+            Canvas.Children.Clear();
+            if (!File.Exists(ImagePath))
+            {
+                return;
+            }
+
+            using (var stream = new FileStream(ImagePath, FileMode.Open, FileAccess.Read))
+            {
+                var frame = BitmapFrame.Create(stream, BitmapCreateOptions.DelayCreation, BitmapCacheOption.None);
+                var k = 1.0 * ImageGrid.ActualWidth / frame.PixelWidth;
+
+                foreach (var line in lines)
+                {
+                    AddSelectionRectangle(
+                        new Point(k * line.X, k * line.Y),
+                        new Point(k * line.X2, k * line.Y2));
+                }
+            }
         }
 
         private void ShowImage(string path)
         {
+            ImagePath = path;
             Image.Source = new BitmapImage(new Uri(path, UriKind.Absolute));
+        }
+
+        private void AddSelectionRectangle(Point point1, Point point2)
+        {
+            var topLeftPoint = new Point(Math.Min(point1.X, point2.X), Math.Min(point1.Y, point2.Y));
+
+            var rectangle = new Rectangle
+            {
+                Stroke = Brushes.Red,
+                StrokeThickness = 1,
+                Margin = new Thickness(topLeftPoint.X, topLeftPoint.Y, topLeftPoint.X, topLeftPoint.Y),
+                Width = Math.Abs(point2.X - point1.X),
+                Height = Math.Abs(point2.Y - point1.Y)
+            };
+            Canvas.Children.Add(rectangle);
         }
     }
 }
