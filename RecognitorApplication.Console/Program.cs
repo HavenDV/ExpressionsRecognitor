b4b3de8a43fdd8c24ff9ced35635a49cd6619427
HavenDV <havendv@gmail.com>
2018-06-08 14:47:42 +1000

    Added Console application

---
+++ RecognitorApplication.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using RecognitionLibrary.Converters;
+
+namespace RecognitorConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = args[0];
+
+            Convert(path);
+        }
+
+        private static async void Convert(string path)
+        {
+            Console.WriteLine("Converting...");
+            var actual = await ImageToLatexConverter.ConvertAsync(path);
+
+            Console.WriteLine("Result:");
+            Console.WriteLine(actual);
+        }
+    }
+}
